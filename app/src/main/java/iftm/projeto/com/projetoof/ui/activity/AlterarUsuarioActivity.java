14e321daea6b5f4ea000239a1419d0a500056d41
Filipe Silva <filipe.silva@zup.com.br>
2018-12-03 17:06:23 -0200

    tela gerenciar criada

---
+++ app/src/main/java/iftm/projeto/com/projetoof/ui/activity/AlterarUsuarioActivity.java
@@ -0,0 +1,15 @@
+package iftm.projeto.com.projetoof.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import iftm.projeto.com.projetoof.R;
+
+public class AlterarUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alterar_usuario);
+    }
+}
--- app/src/main/java/iftm/projeto/com/projetoof/ui/activity/GerenciarUsuariosActivity.java
+++ app/src/main/java/iftm/projeto/com/projetoof/ui/activity/GerenciarUsuariosActivity.java
@@ -4,7 +4,9 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -28,7 +30,7 @@ public class GerenciarUsuariosActivity extends AppCompatActivity implements OnAd
     private OnAddListenner onAddListenner;
     private AdapterUsuario adapter;
     private ListView lista;
-    private Toolbar toolbar;
+    private RelativeLayout toolbar;
 
     @Override
     public void onResume() {
@@ -65,8 +67,8 @@ public class GerenciarUsuariosActivity extends AppCompatActivity implements OnAd
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gerenciar_usuarios);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setVisibility(View.VISIBLE);
 
         // add back arrow to toolbar
         if (getSupportActionBar() != null){
@@ -93,5 +95,10 @@ public class GerenciarUsuariosActivity extends AppCompatActivity implements OnAd
     @Override
     public void onEventoListenner() {
 
+    }
+
+    public void onBackActivity(View view) {
+        toolbar.setVisibility(View.GONE);
+        finish();
     }
 }
